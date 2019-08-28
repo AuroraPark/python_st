da0e1b405e02002ac9228dac2d0f0f125d1eb4cc
AuroraPark <47839204+AuroraPark@users.noreply.github.com>
2019-08-28 23:29:33 +0900

    반복문

---
+++ forEx.py
@@ -0,0 +1,14 @@
+# 단순 반복문
+for x in range(10):
+    print("hello")
+
+# 블록 반복문
+for x in range(3):
+    print(100)
+    print(200)
+print(300)
+
+
+# 반복문 숫자 리스트 - 마지막수 미포함
+for x in range(1, 11):
+    print(x)
