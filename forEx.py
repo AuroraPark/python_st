7140cb50ef0ef399d08b93ecf3c5927f86c1bb81
AuroraPark <47839204+AuroraPark@users.noreply.github.com>
2019-08-29 13:06:26 +0900

    파이썬 반복문 실습

--- forEx.py
+++ forEx.py
@@ -12,3 +12,20 @@ print(300)
 # 반복문 숫자 리스트 - 마지막수 미포함
 for x in range(1, 11):
     print(x)
+
+# 반복문 합계(SUM)
+s = 0
+for x in range(1, 10+1):
+    s = s + x # s += x 도 가능
+    print("x:", x, " s:", s)
+
+# 원그리기
+import turtle as t
+
+n=50
+t.bgcolor("black")
+t.color("green")
+t.speed(0)
+for x in range(n):
+    t.circle(80)
+    t.left(360/n)
