31712c0cb6dadbfc0915cf7f10b2c528476c9dd3
AuroraPark <47839204+AuroraPark@users.noreply.github.com>
2019-08-29 13:29:22 +0900

    if-else 예제

---
+++ ifelseEx.py
@@ -0,0 +1,22 @@
+a =3
+
+if a == 2:
+    print("a는 2")
+
+if a == 3:
+    print("a는 3")
+
+# if else 문
+if a == 2:
+    print("a는 2")
+else:
+    print("a는 2가 아님")
+
+# 덧셈 맞추기 문제
+x = input("12+32=?")
+a = int(x)
+
+if a == 12 + 32:
+    print("정답!")
+else:
+    print("실패!")
